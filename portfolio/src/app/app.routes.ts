b993c37ffbdf8a7eb1374d2432c3851b13857290
Sofiane BADJI <soufyane.badji@smile.fr>
2023-12-04 18:50:03 +0100

    WIP archi + main container

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {MainContainerComponent} from "./components/main-container/main-container.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainContainerComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ portfolio/src/app/components/main-container/main-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-container.component.html',
+  styleUrl: './main-container.component.scss'
+})
+export class MainContainerComponent {
+
+}
