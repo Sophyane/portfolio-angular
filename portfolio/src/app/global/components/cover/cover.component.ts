6c98005f343db9b677ff12f9182990f43e11352b
sofiane <badjisic@gmail.com>
2024-02-01 22:24:47 +0100

    fix tailwind + dark/light mode +  WIP Cover (#5)

---
+++ portfolio/src/app/global/components/cover/cover.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-cover',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './cover.component.html',
+  styleUrl: './cover.component.scss'
+})
+export class CoverComponent {
+
+}
