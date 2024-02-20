af5e2d31d3f7ac7e032902eac3d58463780eadfc
Sophyane <48772206+Sophyane@users.noreply.github.com>
2024-02-20 17:40:49 +0100

    WIP lang and hobbies disposition

--- portfolio/src/app/global/components/lang-hobbies/lang-hobbies.component.ts
+++ portfolio/src/app/global/components/lang-hobbies/lang-hobbies.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { EmojiComponent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+import { MatDivider } from '@angular/material/divider';
+import { Hobby, Language } from '../../models/global.model';
 
 @Component({
   selector: 'app-lang-hobbies',
   standalone: true,
-  imports: [],
+  imports: [PickerComponent, FormsModule, NgIf, EmojiComponent, MatDivider],
   templateUrl: './lang-hobbies.component.html',
-  styleUrl: './lang-hobbies.component.scss'
+  styleUrl: './lang-hobbies.component.scss',
 })
 export class LangHobbiesComponent {
+  languages: Language[] = [
+    { name: 'français', emoji: 'baguette_bread', level: 'C2' },
+    { name: 'anglais', emoji: 'male-guard', level: 'C1' },
+    { name: 'arabe', emoji: 'camel', level: 'C2' },
+    { name: 'italien', emoji: 'spaghetti', level: 'B1' },
+  ];
 
+  hobbies: Hobby[] = [
+    { name: 'football', emoji: 'soccer' },
+    { name: 'tennis', emoji: 'tennis' },
+    { name: 'gaming', emoji: 'video_game' },
+    { name: 'hiking', emoji: 'hiking_boot' },
+    { name: 'art', emoji: 'art' },
+    { name: 'surf-skate', emoji: 'skateboard' },
+    { name: 'cinema', emoji: 'popcorn' },
+    { name: 'guitare / Ukulele', emoji: 'guitar' },
+  ];
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,28 +1,24 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  darkMode: 'class',
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'slate-200': '#E5E7EB',
-        'slate-700': '#374151',
-        'teal-600': '#047857',
-        'slate-900': '#1F2937',
+        "slate-200": "#E5E7EB",
+        "slate-700": "#374151",
+        "teal-600": "#047857",
+        "slate-900": "#1F2937",
       },
       screens: {
-        'xs': '320px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+        xs: "320px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
