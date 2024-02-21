a0296cec0764bc55d8bc52e856b8b5f3c5b0ba02
BADJI Sofiane <badjisic@gmail.com>
2024-02-21 21:07:24 +0100

    Lang hobbies (#8)
    * WIP lang and hobbies disposition
    
    * hobbies ready

---
+++ portfolio/src/app/global/models/global.model.ts
@@ -0,0 +1,10 @@
+export interface Language {
+  readonly name: string;
+  readonly emoji: string;
+  readonly level: string;
+}
+
+export interface Hobby {
+  readonly name: string;
+  readonly emoji: string;
+}
