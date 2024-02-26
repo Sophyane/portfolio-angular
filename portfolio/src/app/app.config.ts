5e65d2e3ab506efcb16965010cd5c45f85e02aaf
Sophyane <48772206+Sophyane@users.noreply.github.com>
2024-02-26 18:51:51 +0100

    install transloco

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,14 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideAnimations(),
+    provideAnimations(), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
   ],
 };
